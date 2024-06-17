2cf2c5e076efe8a053e881b730873bb7f276fad6
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-06-18 03:13:29 +0530

    Starts Customizer component, required components created

--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -1,6 +1,14 @@
 // Exporting all compoenent together
 import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab"
 
 export{
-    CustomButton
+    CustomButton,
+    AIPicker,
+    ColorPicker,
+    FilePicker,
+    Tab
 }
--- Client/src/pages/Customizer.jsx
+++ Client/src/pages/Customizer.jsx
@@ -1,4 +1,14 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useSnapshot } from 'valtio'
+
+import config from '../config/config.js'
+import state from '../store/index.js'
+import {download } from '../assets'
+import {downloadCanvasToImage, reader} from '../config/helpers.js'
+import {EditorTabs, FilterTabs, DecalTypes} from '../config/constants.js'
+import { fadeAnimation, slideAnimation } from '../config/motion'
+import {AIPicker,ColorPicker,FilePicker, Tab,CustomButton} from "../components"
 
 const Customizer = () => {
   return (
