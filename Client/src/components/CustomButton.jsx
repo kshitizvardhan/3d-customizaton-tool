16e02852d298de67664e7a8adfa18e07f56c729a
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-06-18 02:08:30 +0530

    Custom Button Component

---
+++ Client/src/components/CustomButton.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import state from '../store'
+import { useSnapshot } from 'valtio'
+
+const CustomButton = ({title, type, customStyles, handleClick}) => {
+    const snap = useSnapshot(state);
+    const generateStyle = (type) => {
+        if(type == "filled"){
+            return{
+                backgroundColor: snap.color,
+                color: "#fff"
+            }
+        }
+    }
+    
+  return (
+    <button className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`} style={generateStyle(type)} onClick={handleClick}>{title}</button>
+  )
+}
+
+// As soon as the button is clicked the handleClick sets the intro in state to false and hence we no longer on the Home Page
+// After button is clicked we will move to Customizer-page
+
+export default CustomButton
