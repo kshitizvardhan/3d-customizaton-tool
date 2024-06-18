6818b3714f331f129d8399709189ddde96f22f7c
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-06-19 02:47:01 +0530

    Add AI-Picker Component

--- Client/src/components/AIPicker.jsx
+++ Client/src/components/AIPicker.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import CustomButton from "./CustomButton"
 
-const AIPicker = () => {
+const AIPicker = ({prompt, setPrompt, generatingImg, handleSubmit}) => {
   return (
-    <div>AIPicker</div>
+    <div className='aipicker-container'>
+      <textarea id="" 
+        placeholder='Ask AI..'
+        rows={5}
+        value={prompt}
+        onChange={(e)=>setPrompt(e.target.value)}
+        className='aipicker-textarea'
+      />
+      <div className='flex flex-wrap gap-3'>
+        {generatingImg ? (
+          <CustomButton 
+            type={"outline"}
+            title={"Asking AI"}
+            customStyles={"text-xs"}
+          />
+        ) : (
+          <>
+            <CustomButton 
+              type={"outline"}
+              title={"Logo"}
+              handleClick={() => handleSubmit('logo')}
+              customStyles={"text-xs"}
+            />
+            <CustomButton 
+              type={"filled"}
+              title={"Full"}
+              handleClick={() => handleSubmit('logo')}
+              customStyles={"text-xs"}
+            />
+          </>
+        )}
+      </div>
+    </div>
   )
 }
 
