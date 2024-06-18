30f752a7afd27258729945b704a456fc62b4b377
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-06-19 02:30:35 +0530

    Add backend, routes and api

--- Server/index.js
+++ Server/index.js
@@ -2,9 +2,12 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+import dalleRoutes from "./routes/dalle.routes.js"
+
 dotenv.config();
 
 const app = express();
+const port = 8080;
 
 app.use(cors());  
 
@@ -13,10 +16,12 @@ app.use(express.json({
     limit: "50mb"
 }));
 
+app.use("/api/v1/dalle", dalleRoutes);
+
 app.get("/",(req,res) => {
     res.status(200).json({
         message: "Hello from DALL.E"
     })
 })
 
-app.listen(8080, () => console.log("Server has started on port 8080"));
+app.listen(port, () => console.log(`Server has started on port ${port}`));
